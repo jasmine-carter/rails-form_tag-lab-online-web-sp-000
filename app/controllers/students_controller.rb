5901882613fb39e9109aba64568143bf895f12ea
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-03 00:43:58 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,8 +8,8 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def create
+  def new
     Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
   end
-  
+
 end
